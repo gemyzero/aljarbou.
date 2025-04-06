9a315a33e6df3b3092cce01c78e31d0aff659205
gemyzero <mohamed0110gemy@gmail.com>
2025-04-06 04:59:57 +0200

    Fix: Resolve Logo1.png import error

--- src/Componets/header/Navbar.jsx
+++ src/Componets/header/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../../public/images/Logo1.png'
+
 
 export default function Navbar() {
   return (
@@ -10,7 +10,7 @@ export default function Navbar() {
       <div className='N'> 
      <div className='navbar  px-4 py-lg-3  py-md-4'>
      <div className=" logo  py-lg-1  px-lg-5">
-            <img src={logo} alt="" />
+            <img src='../../../public/images/Logo1.png'  alt="" />
         </div>  
         <div className= " py-lg-1  start-for-free me-lg-5 px-lg-5">
             <button className=' '>ابدأ مجانًا            </button>
