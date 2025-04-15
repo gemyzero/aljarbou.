9e4f4488fc200b178feb696ef17f2b45f098f54c
gemyzero <mohamed0110gemy@gmail.com>
2025-04-16 00:29:34 +0200

    finsh an

--- src/Componets/higher/Higher.jsx
+++ src/Componets/higher/Higher.jsx
@@ -13,7 +13,7 @@ export default function Higher() {
         // اختيارات التهيئة (اختيارية)
         duration: 1000, // مدة الحركة بالمللي ثانية
         easing: 'ease-in-out', // نوع الحركة (ease, ease-in, ease-out, linear, الخ.)
-        once: true, // هل يتم تشغيل الحركة مرة واحدة فقط؟
+        once: false, // هل يتم تشغيل الحركة مرة واحدة فقط؟
       });
     }, []);
   return (
